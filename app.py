c76032f2eaf646b31c9814b554c8eb9885dcce55
sarralabidi <sarra.labidi@esprit.tn>
2025-11-02 11:13:03 +0100

    personalization

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello, World! This Flask app is containerized."
+    return "Hello, Opus Lab! This Flask app is containerized by me Sarra!."
 
 
 if __name__ == "__main__":
